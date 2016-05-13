69fc54f1175e280af1cc5c6700092339abbedcd4
yzzhou <yzzhou@thoughtworks.com>
2016-05-13 23:54:20 +0800

    extract release plan to single file

---
+++ src/components/ReleaseList.js
@@ -0,0 +1,59 @@
+/**
+ * Created by yzzhou on 5/13/16.
+ */
+
+import React from 'react';
+
+var ReleaseList = React.createClass({
+    createNoteNode(factList) {
+        return (
+            <ul>
+                {factList.map(function (fact, index) {
+                    return (
+                        <li key={'fact-note-' + index}>
+                            {fact.impactedNote + '(' + fact.impactedPoints + ' point' + ((Math.abs(fact.impactedPoints) > 1) ? 's': '') + ')'}
+                        </li>);
+                })}
+            </ul>
+        );
+    },
+    createReleaseNode(release) {
+        return (
+            <tr key={release.id}>
+                <td>{release.get('name')}</td>
+                <td>{release.get('scope')}</td>
+                <td>{release.get('developmentIterations')}</td>
+                <td>{release.get('regressionIterations')}</td>
+                <td>{release.get('buffer')}</td>
+                <td>{release.get('bestReleaseDate')}</td>
+                <td>{release.get('worstReleaseDate')}</td>
+                <td>{this.createNoteNode(release.get('factList'))}</td>
+                <td>Actions</td>
+            </tr>
+        );
+    },
+    render() {
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th>Release Name</th>
+                    <th>Scope</th>
+                    <th>Development Iterations</th>
+                    <th>Regression Iterations</th>
+                    <th>Buffer</th>
+                    <th>Release Date(Best)</th>
+                    <th>Release Date(Worst)</th>
+                    <th>Note</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.props.releases.map(this.createReleaseNode)}
+                </tbody>
+            </table>
+        );
+    }
+});
+
+module.exports = ReleaseList;
--- src/components/releasePlan.js
+++ src/components/releasePlan.js
@@ -7,59 +7,8 @@ import ReactDOM from 'react-dom';
 import ReleasePlanStore from '../stores/ReleasePlanStore';
 import Settings from './Settings';
 import ReleaseForm from './ReleaseForm';
-import ActionFactory from '../actions/ActionFactory';
+import ReleaseList from './ReleaseList';
 
-var ReleaseList = React.createClass({
-    createNoteNode(factList) {
-        return (
-            <ul>
-            {factList.map(function (fact, index) {
-                return (
-                <li key={'fact-note-' + index}>
-                    {fact.impactedNote + '(' + fact.impactedPoints + ' point' + ((Math.abs(fact.impactedPoints) > 1) ? 's': '') + ')'}
-                </li>);
-            })}
-            </ul>
-        );
-    },
-    createReleaseNode(release) {
-        return (
-            <tr key={release.id}>
-                <td>{release.get('name')}</td>
-                <td>{release.get('scope')}</td>
-                <td>{release.get('developmentIterations')}</td>
-                <td>{release.get('regressionIterations')}</td>
-                <td>{release.get('buffer')}</td>
-                <td>{release.get('bestReleaseDate')}</td>
-                <td>{release.get('worstReleaseDate')}</td>
-                <td>{this.createNoteNode(release.get('factList'))}</td>
-                <td>Actions</td>
-            </tr>
-        );
-    },
-    render() {
-       return (
-           <table>
-               <thead>
-               <tr>
-                   <th>Release Name</th>
-                   <th>Scope</th>
-                   <th>Development Iterations</th>
-                   <th>Regression Iterations</th>
-                   <th>Buffer</th>
-                   <th>Release Date(Best)</th>
-                   <th>Release Date(Worst)</th>
-                   <th>Note</th>
-                   <th>Actions</th>
-               </tr>
-               </thead>
-               <tbody>
-                   {this.props.releases.map(this.createReleaseNode)}
-               </tbody>
-           </table>
-       );
-    }
-});
 
 var ReleasePlan = React.createClass({
     getInitialState() {
