7de15b514958f518cdb64b8ed0f21888482f6f94
yzzhou <yzzhou@thoughtworks.com>
2016-05-17 14:59:06 +0800

    make gulp watch work even there are errors

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
  * Created by yzzhou on 5/12/16.
  */
 var gulp = require('gulp');
+var gutil = require("gulp-util");
 var browserify = require("browserify");
 var path =require('path');
 var fs = require('fs');
@@ -20,8 +21,15 @@ gulp.task('build', function () {
         .transform("babelify", {presets: ["es2015", "react"]})
         .bundle()
         .pipe(source('App.js'))
+        .on('error', onError)
         .pipe(gulp.dest('./src/'));
         // .pipe(fs.createWriteStream('./src/App.js));
         // .pipe(gulp.dest(path.resolve(__dirname, './src/App.js')));
 });
+
+function onError() {
+    gutil.log(gutil.colors.red("ERROR", taskName), err);
+    gutil.log(new gutil.PluginError(taskName, err, { showStack: true }));
+    this.emit("end");
+}
 
